ff7b987dec161d024dd3bad97d2c5ed0a1078d9a
Tunde Nasri <tundenasri@gmail.com>
2021-05-08 18:35:59 +0100

    update project contents

--- pages/components/modules/Hero.js
+++ pages/components/modules/Hero.js
@@ -10,22 +10,25 @@ const Hero = () => {
           </h3>
           <p className="leading-loose font-light lg:w-4/5 text-lg mb-4">
             My name is Babatunde Yakub, a FullStack Engineer. I’m a systems
-            thinker who can work with a team to transform complex workflows into
-            intuitive technology. I have a history of Web development and
-            building complex systems from the ground-up. What can I do for you? I
-            can convert your idea into a product. Do you have a project I can
-            help you with? <a href="#contact" className='underline'>Contact me</a>
+            thinker who can work with creative teams to transform complex
+            workflows into intuitive technology. I am versatile in web
+            development and building complex systems from the ground-up. What
+            can I do for you? I'm the go-to-person to convert your ideas into a
+            product. Do you have a project I can help you with?{" "}
+            <a href="#contact" className="underline">
+              Contact me
+            </a>
           </p>
           <div>
             <div className="flex">
-              <Badge text="Frontend" icon='code'/>
-              <Badge text="Backend" icon='server' />
+              <Badge text="Frontend" icon="code" />
+              <Badge text="Backend" icon="server" />
             </div>
             <Badge text="Cloud Enthusiast (I love AWS)" icon="cloud" />
           </div>
           {/* <img src="./tunde.png" alt="" className='sm:hidden mt-4'/> */}
         </div>
-        <img src="./hero.svg" alt="" className='hidden lg:block w-2/4' />
+        <img src="./hero.svg" alt="" className="hidden lg:block w-2/4" />
       </div>
     </div>
   );
--- pages/components/modules/Projects.js
+++ pages/components/modules/Projects.js
@@ -12,26 +12,26 @@ const Projects = () => {
         <Project
           title="Fibre LifeStyle"
           desc="Fibre LifeStyle is a real etstate company in Lagos, I joined as a Frontend Developer, and I have been working closely with 
-          designers and other developers in the team for a period of over 1.5years now. I also developed a complex backend API that's consumed by a partner company, Bento. The goal of the solution is to
+          designers and other developers in the team to manage the comapany's client and admin application for over 1.5 years. 
+          I also developed a complex backend API that's consumed by a partner company, Bento. The goal of the solution is to
           have disbursed over 100 million naira in rent loans to Nigierians and Ghananians by the end of 2021."
           image="fibre.svg"
           webLink="https://fibre.ng"
         />
-        <hr className='sm:hidden w-3/6 m-auto' />
+        <hr className="sm:hidden w-3/6 m-auto" />
         <RtlProject
           title="Jackocoins"
-          desc="Jackocoins is a business that started on Whatsapp, buying and selling cryptocurrencies. The business soon became too large and complex to manage,
-          the business owner reached out and we developed a client facing mobile and web app, we also developed a robust admin app. Since early 2021, Jackocoins
-          users transacts over 200 million naira daily."
+          desc="Jackocoins is a cryptocurrency exchange platform that started on WhatsApp. With complexities and management difficulties, 
+          we were engaged to develop a client-facing mobile and web app, and a robust admin app. Since early 2021, Jackocoins users transacts over 50 million naira daily."
           image="jackocoins.svg"
           webLink="https://jackocoins.com"
           mobileLink="https://play.google.com/store/apps/details?id=com.jackocoins.app"
         />
-        <hr className='sm:hidden w-3/6 m-auto'  />
+        <hr className="sm:hidden w-3/6 m-auto" />
         <Project
           title="SideHustle"
-          desc="After a 3 days Hackothon with Wema Bank in 2019 (Hackaholics), I and my team were called back to build the solution we pitched to them, we 
-          spent 4 months, incubated by the bank where we developed a mobile solution and I led the Frontend Development"
+          desc="SideHustle was conceptualized during a 72-hour programming hackathon (2019 Hackaholics by Wema Bank). 
+          The impressive pitch by my team led to our selection as participants in the 4-month boot camp for full-scale product development. I led the Frontend Development of the mobile solution for the product."
           image="sidehustle.jpg"
           mobileLink="https://play.google.com/store/apps/details?id=com.sidehustle&hl=en&gl=US"
         />
